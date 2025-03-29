771621d6d4026cbf43911c5b1d38b001092c1c2d
Yerkhatik <98648372+Yerkhatik@users.noreply.github.com>
2025-03-29 11:47:17 +0500

    lab7

---
+++ Lan6/project/src/app/models.ts
@@ -0,0 +1,14 @@
+export interface Album{
+    userId: number,
+    id: number,
+    title: string
+  }
+  
+  export interface Photos{
+    albumId: number,
+    id: number,
+    title: string,
+    url: string,
+    thumbnailUrl: string
+  }
+  
